fa5d984d0c6710e7eb49182d4f260f0101cbbc41
BIGDATA21C <bigdatasolutions.21c@gmail.com>
2018-12-03 12:52:25 +0530

    removed all files

--- src/app/pipes/foldersearch/foldersearch.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FoldersearchPipe } from './foldersearch.pipe';
-
-describe('FoldersearchPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FoldersearchPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- src/app/pipes/prelims/prelimsfilter.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PrelimsfilterPipe } from './prelimsfilter.pipe';
-
-describe('PrelimsfilterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new PrelimsfilterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
