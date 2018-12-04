b07b22fb118b9359c53ab255dcf6c1a30eab5299
BIGDATA21C <bigdatasolutions.21c@gmail.com>
2018-12-04 18:45:27 +0530

    Removed all files

--- src/app/models/quotes.ts
+++
@@ -1,8 +0,0 @@
-// create an interface -- object -- named arrays not like 0,1,2,3
-
-export class Quotes {
-    _id?:string;
-    author:string;
-    category:string;
-    quotedesc:string; 
-}
--- src/app/pipes/essay/essayfilter.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { EssayfilterPipe } from './essayfilter.pipe';
-
-describe('EssayfilterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new EssayfilterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
