c6259b1c0d0ac56d6676601e8e812bcbf809d2f9
BIGDATA21C <bigdatasolutions.21c@gmail.com>
2018-12-08 19:08:11 +0530

    Q

--- src/app/pipes/optionals/optionals.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { OptionalsPipe } from './optionals.pipe';
-
-describe('OptionalsPipe', () => {
-  it('create an instance', () => {
-    const pipe = new OptionalsPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
